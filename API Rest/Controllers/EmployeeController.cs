73876a1e7f0ef2330f3af87628aa6b0122ddc7ad
Ángel Tejeda <angel.tejedat@gmail.com>
2021-05-07 19:32:58 -0500

    Changed endpoint. /Employee/some/{}  -> /Employee/pages/{page}. Modified some functionality in the endpoint.

--- API Rest/Controllers/EmployeeController.cs
+++ API Rest/Controllers/EmployeeController.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Tarea_4;
 using Tarea_4.BackEnd;
 using Tarea_4.DataAccess;
 using Tarea_4.Models;
@@ -14,94 +14,60 @@ namespace API_Rest.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        private IActionResult InternalServerError(Exception ex)
+        private IActionResult InternalServerError(string errorMessage = "")
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError, errorMessage);
         }
 
-        // GET: api/<EmployeeController>
-        [HttpGet]
-        [Route("all")]
-        public IActionResult GetAll()
+        private readonly string InstanceName = "employee";
+
+        // GET api/<EmployeeController>/{id}
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
         {
-            List<EmployeePersonalInfoDTO> employees = new();
+            if (id < 1)
+                return BadRequest($"{nameof(id)} must be at least 1.");
 
-            using (NorthwindContext dbContext = new())
-            {
-                IQueryable<Employee> dbEmployees = EmployeeSC.GetAllEmployees(dbContext).AsNoTracking();
+            Employee dbEmployee = new EmployeeSC().GetEmployeeById(id);
 
-                foreach (Employee dbEmployee in dbEmployees)
-                {
-                    employees.Add(new EmployeePersonalInfoDTO(dbEmployee));
-                };
-            }
+            if (dbEmployee == null)
+                return NotFound(DbExceptionMessages.InstanceNotFound(InstanceName, id));
 
-            return Ok(employees);
+            EmployeePersonalInfoDTO employee = new(dbEmployee);
+
+            return Ok(employee);
         }
 
+        // GET: api/<EmployeeController>/page/{page}
         [HttpGet]
-        [Route("some/{page}")]
-        public IActionResult GetSome(int page)
+        [Route("page/{page}")]
+        public IActionResult GetPage(int page)
         {
             const int elementsPerPage = 10;
-            Pagination<EmployeePersonalInfoDTO> response = new();
 
-            List<EmployeePersonalInfoDTO> employees = new();
-            using (NorthwindContext dbContext = new())
-            {
-                int totalElements = EmployeeSC.CountEmployees(dbContext);
-                int maxPage = Convert.ToInt32(Math.Ceiling((double)totalElements / elementsPerPage));
+            if (page < 1)
+                return BadRequest($"{nameof(page)} must be at least 1.");
 
-                if (page < 1)
-                    BadRequest();
+            int lastPage = new EmployeeSC().CalculateLastPage(elementsPerPage);
+            Pagination<EmployeePersonalInfoDTO> response = new(page, lastPage);
 
-                if (page > maxPage)
-                    page = maxPage;
-
-                response.PreviousPage = page - 1;
-                response.CurrentPage = page;
-                response.NextPage = page + 1;
-
-                if (page == maxPage)
-                    response.NextPage = null;
-
-                if (page == 1)
-                    response.PreviousPage = null;
-
-                IQueryable<Employee> dbEmployees = EmployeeSC.GetSomeEmployees(dbContext, page);
-
-                foreach (Employee dbEmployee in dbEmployees)
-                {
-                    employees.Add(new EmployeePersonalInfoDTO(dbEmployee));
-                };
-            }
+            IQueryable<Employee> dbEmployees = new EmployeeSC().GetPage(elementsPerPage, response.CurrentPage);
+            List<EmployeePersonalInfoDTO> employees = EmployeeSC.MaterializeIQueryable<EmployeePersonalInfoDTO>(dbEmployees);
 
             response.ResponseList = employees;
 
             return Ok(response);
         }
 
-        // GET api/<EmployeeController>/5
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            try
-            {
-                EmployeePersonalInfoDTO employee;
+            IQueryable<Employee> dbEmployees = new EmployeeSC().GetAllEmployees();
 
-                using (NorthwindContext dbContext = new())
-                {
-                    Employee dbEmployee = EmployeeSC.GetEmployeeById(dbContext, id);
+            List<EmployeePersonalInfoDTO> employees = EmployeeSC.MaterializeIQueryable<EmployeePersonalInfoDTO>(dbEmployees);
 
-                    employee = new(dbEmployee);
-                }
-
-                return Ok(employee);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Ok(employees);
         }
 
         // POST api/<EmployeeController>
@@ -109,58 +75,75 @@ namespace API_Rest.Controllers
         public IActionResult Post([FromBody] EmployeePersonalInfoDTO newEmployee)
         {
             int id;
-
+            
             try
             {
-                using (NorthwindContext dbContext = new())
-                {
-                    id = EmployeeSC.AddNewEmployee(dbContext, newEmployee);
-                }
-
-                return Ok("Registered Id: " + id);
+                id = new EmployeeSC().AddNewEmployee(newEmployee);
             }
-            catch (Exception ex)
+            catch(Exception ex) when (ExceptionTypes.IsSqlException(ex))
             {
-                return InternalServerError(ex);
+                return Conflict(ex.InnerException.Message);
             }
+            catch(Exception ex) when (ExceptionTypes.IsDbException(ex))
+            {
+                return InternalServerError();
+            }
+
+            return Ok(id);
         }
 
-        // PUT api/<EmployeeController>/5
+        // PUT api/<EmployeeController>/{id}
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] EmployeePersonalInfoDTO modifiedEmployee)
         {
+            if (modifiedEmployee.Id != id)
+                return BadRequest($"{nameof(id)} and {nameof(modifiedEmployee.Id)} must have the same value.");
+            
+            Employee dataBaseEmployee = new EmployeeSC().GetEmployeeById(id);
+
+            if (dataBaseEmployee == null)
+                return NotFound(DbExceptionMessages.InstanceNotFound(InstanceName, id));
+
             try
             {
-                using (NorthwindContext dbContext = new())
-                {
-                    EmployeeSC.UpdateEmployee(dbContext, id, modifiedEmployee);
-                }
-
-                return Ok();
+                //TODO: Check if it is possible to pass the dataBaseEmployee insted of the id.
+                new EmployeeSC().UpdateEmployee(id, modifiedEmployee);
             }
-            catch (Exception ex)
+            catch (Exception ex) when (ExceptionTypes.IsSqlException(ex))
             {
-                return InternalServerError(ex);
+                return Conflict(ex.InnerException.Message);
             }
+            catch (Exception ex) when (ExceptionTypes.IsDbException(ex))
+            {
+                return InternalServerError();
+            }
+
+            return NoContent();
         }
 
-        // DELETE api/<EmployeeController>/5
+        // DELETE api/<EmployeeController>/{id}
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            Employee dataBaseEmployee = new EmployeeSC().GetEmployeeById(id);
+
+            if (dataBaseEmployee == null)
+                return NotFound(DbExceptionMessages.InstanceNotFound(InstanceName, id));
+
             try
             {
-                using (NorthwindContext dbContext = new())
-                {
-                    EmployeeSC.DeleteEmployee(dbContext, id);
-                }
-
-                return Ok();
+                new EmployeeSC().DeleteEmployee(dataBaseEmployee);
             }
-            catch (Exception ex)
+            catch (Exception ex) when (ExceptionTypes.IsSqlException(ex))
             {
-                return InternalServerError(ex);
+                return Conflict(ex.InnerException.Message);
             }
+            catch (Exception ex) when (ExceptionTypes.IsDbException(ex))
+            {
+                return InternalServerError();
+            }
+
+            return NoContent();
         }
     }
 }
