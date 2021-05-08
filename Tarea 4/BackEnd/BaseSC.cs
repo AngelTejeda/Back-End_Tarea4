f96b709c8961094f2c32e23baa6c9355b90f302f
Ángel Tejeda <angel.tejedat@gmail.com>
2021-05-07 19:31:39 -0500

    EmployeeSC is no longer a static class.

---
+++ Tarea 4/BackEnd/BaseSC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea_4.DataAccess;
+
+namespace Tarea_4.BackEnd
+{
+    public class BaseSC
+    {
+        protected NorthwindContext dbContext = new();
+    }
+}
--- Tarea 4/BackEnd/EmployeeSC.cs
+++ Tarea 4/BackEnd/EmployeeSC.cs
@@ -1,117 +1,182 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Tarea_4.DataAccess;
 using Tarea_4.Models;
 
 namespace Tarea_4.BackEnd
 {
-    public static class EmployeeSC
+    public class EmployeeSC : BaseSC
     {
-        private static readonly string InstanceName = "employee";
-        
-        public static int CountEmployees(NorthwindContext dbContext)
+        /// <summary>
+        /// Materializes a given IQueryable of employees into a List.
+        /// </summary>
+        /// <typeparam name="T"> A Data Transfer Object Class of the employees.</typeparam>
+        /// <param name="dataBaseEmployees">IQueryable object of the employees that will be materialized.</param>
+        /// <returns>A List<typeparamref name="T"/> with the materialized employees.</returns>
+        public static List<T> MaterializeIQueryable<T>(IQueryable<Employee> dataBaseEmployees) where T : EmployeeDTO, new()
         {
-            return GetAllEmployees(dbContext).Count();
+            List<T> employees = new();
+
+            dataBaseEmployees = dataBaseEmployees.AsNoTracking();
+
+            foreach (Employee dbEmployee in dataBaseEmployees)
+            {
+                T dtoObject = new();
+                dtoObject.CopyInfoFromDataBaseEmployee(dbEmployee);
+
+                employees.Add(dtoObject);
+            };
+
+            return employees;
         }
 
-        public static IQueryable<Employee> GetAllEmployees(NorthwindContext dbContext)
+        /// <summary>
+        /// Returns the total amount of employees in the DataBase.
+        /// </summary>
+        /// <returns>The total amount of employees in the DataBase.</returns>
+        public int CountEmployees()
         {
-            return dbContext.Employees.AsQueryable();
+            return GetAllEmployees().Count();
         }
 
-        public static IQueryable<Employee> GetSomeEmployees(NorthwindContext dbContext, int page)
+        /// <summary>
+        /// Gets the employee with the specified id.
+        /// </summary>
+        /// <param name="id">Id of the employee.</param>
+        /// <returns>The employee object or null if <paramref name="id"/> doesn't exist.</returns>
+        public Employee GetEmployeeById(int id)
         {
-            const int elementsPerPage = 10;
+            return GetAllEmployees().FirstOrDefault(employee => employee.EmployeeId == id);
+        }
 
-            return GetAllEmployees(dbContext)
+        /// <summary>
+        /// Given a certain number of elements per page, calculates the number of the last page.
+        /// </summary>
+        /// <param name="elementsPerPage">Maximum number of elements that contains a page.</param>
+        /// <returns>The value of the last page.</returns>
+        public int CalculateLastPage(int elementsPerPage)
+        {
+            int totalElements = CountEmployees();
+            int lastPage = Convert.ToInt32(Math.Ceiling((double)totalElements / elementsPerPage));
+
+            return lastPage;
+        }
+
+        /// <summary>
+        /// Returns only the specified amount of employees in a certain page.
+        /// The number of pages is calculated with the total amount of employees and the number of employees per page.
+        /// The pages start at 1.
+        /// </summary>
+        /// <param name="elementsPerPage">The number of employees in a page.</param>
+        /// <param name="page">The number of the page that will be retrieved.</param>
+        /// <returns>An IQueryable with the selected employees.</returns>
+        public IQueryable<Employee> GetPage(int elementsPerPage, int page)
+        {
+            if (elementsPerPage <= 0)
+                throw new ArgumentOutOfRangeException(nameof(elementsPerPage));
+
+            if (page <= 0)
+                throw new ArgumentOutOfRangeException(nameof(page));
+
+            return GetAllEmployees()
                 .Skip((page - 1) * elementsPerPage)
                 .Take(elementsPerPage);
         }
 
-        public static Employee GetEmployeeById(NorthwindContext dbContext, int id)
+        /// <summary>
+        /// Returns an IQueryable of all the employees in the DataBase.
+        /// </summary>
+        public IQueryable<Employee> GetAllEmployees()
         {
-            try
-            {
-                return GetAllEmployees(dbContext).First(employee => employee.EmployeeId == id);
-            }
-            catch (ArgumentNullException ex)
-            {
-                ex.SetMessage(DbExceptionMessages.FieldIsRequired("id"));
-                throw;
-            }
-            catch (InvalidOperationException ex)
-            {
-                ex.SetMessage(DbExceptionMessages.InstanceNotFound(InstanceName, id));
-                throw;
-            }
+            return dbContext.Employees.AsQueryable();            
         }
 
-        public static int AddNewEmployee(NorthwindContext dbContext,  EmployeeDTO newEmployee)
+        /// <summary>
+        /// Add a new record of an employee to the DataBase.
+        /// </summary>
+        /// <param name="newEmployee">Model of the employee being registered.</param>
+        /// <returns>The id of the registered employee.</returns>
+        public int AddNewEmployee(EmployeeDTO newEmployee)
         {
-            try
-            {
-                Employee dataBaseEmployee = newEmployee.GetDataBaseEmployeeObject();
+            if (newEmployee == null)
+                throw new ArgumentNullException(nameof(newEmployee));
 
-                dbContext.Employees.Add(dataBaseEmployee);
-                dbContext.SaveChanges();
+            Employee dataBaseEmployee = newEmployee.GetDataBaseEmployeeObject();
 
-                return dataBaseEmployee.EmployeeId;
-            }
-            catch (Exception ex) when (ExceptionTypes.IsSqlException(ex))
-            {
-                ex.SetMessage(DbExceptionMessages.FailedToAdd(InstanceName, ex.InnerException));
-                throw;
-            }
-            catch (Exception ex) when (ExceptionTypes.IsDbException(ex))
-            {
-                ex.SetMessage(DbExceptionMessages.UnexpectedFailure(ex));
-                throw;
-            }
+            dbContext.Employees.Add(dataBaseEmployee);
+            dbContext.SaveChanges();
 
+            return dataBaseEmployee.EmployeeId;
         }
 
-        public static void UpdateEmployee(NorthwindContext dbContext, int id, EmployeeDTO modifiedEmployee)
+        /// <summary>
+        /// Modifies the information of an employee in the DataBase.
+        /// </summary>
+        /// <param name="id">Id of the employee being modified.</param>
+        /// <param name="modifiedEmployee">Model with the new information of the employee.</param>
+        public void UpdateEmployee(int id, EmployeeDTO modifiedEmployee)
         {
-            try
-            {
-                Employee dataBaseEmployee = GetEmployeeById(dbContext, id);
+            if (modifiedEmployee == null)
+                throw new ArgumentNullException(nameof(modifiedEmployee));
 
-                modifiedEmployee.ModifyDataBaseEmployee(dataBaseEmployee);
+            Employee dataBaseEmployee = GetEmployeeById(id);
 
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex) when (ExceptionTypes.IsSqlException(ex))
-            {
-                ex.SetMessage(DbExceptionMessages.FailedToUpdate(InstanceName, id, ex.InnerException));
-                throw;
-            }
-            catch (Exception ex) when (ExceptionTypes.IsDbException(ex))
-            {
-                ex.SetMessage(DbExceptionMessages.UnexpectedFailure(ex));
-                throw;
-            }
+            if (dataBaseEmployee == null)
+                throw new KeyNotFoundException();
+
+            modifiedEmployee.ModifyDataBaseEmployee(dataBaseEmployee);
+
+            dbContext.SaveChanges();
         }
 
-        public static void DeleteEmployee(NorthwindContext dbContext, int id)
+        /// <summary>
+        /// Modifies the information of an employee in the DataBase.
+        /// </summary>
+        /// <param name="dataBaseEmployee">Employee object in the DataBase.</param>
+        /// <param name="modifiedEmployee">Model with the new information of the employee.</param>
+        public void UpdateEmployee(Employee dataBaseEmployee, EmployeeDTO modifiedEmployee)
         {
-            try
-            {
-                Employee dataBaseEmployee = GetEmployeeById(dbContext, id);
+            if (dataBaseEmployee == null)
+                throw new ArgumentNullException(nameof(dataBaseEmployee));
 
-                dbContext.Employees.Remove(dataBaseEmployee);
+            if (modifiedEmployee == null)
+                throw new ArgumentNullException(nameof(modifiedEmployee));
 
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex) when (ExceptionTypes.IsSqlException(ex))
-            {
-                ex.SetMessage(DbExceptionMessages.FailedToDelete(InstanceName, id, ex.InnerException));
-                throw;
-            }
-            catch (Exception ex) when (ExceptionTypes.IsDbException(ex))
-            {
-                ex.SetMessage(DbExceptionMessages.UnexpectedFailure(ex));
-                throw;
-            }
+            modifiedEmployee.ModifyDataBaseEmployee(dataBaseEmployee);
+
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Deletes the record of an employee in the DataBase.
+        /// </summary>
+        /// <param name="id">Id of the employee being removed.</param>
+        public void DeleteEmployee(int id)
+        {
+            Employee dataBaseEmployee = GetEmployeeById(id);
+
+            if (dataBaseEmployee == null)
+                throw new KeyNotFoundException();
+
+            dbContext.Employees.Remove(dataBaseEmployee);
+
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Deletes the record of an employee in the DataBase.
+        /// </summary>
+        /// <param name="dataBaseEmployee">Employee object in the DataBase.</param>
+        public void DeleteEmployee(Employee dataBaseEmployee)
+        {
+            if (dataBaseEmployee == null)
+                throw new ArgumentNullException(nameof(dataBaseEmployee));
+
+            dbContext.Employees.Remove(dataBaseEmployee);
+
+            dbContext.SaveChanges();
         }
     }
 }
